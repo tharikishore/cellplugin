1dfb018152213fdec3b44ad1f85e89a1d2a33327
Harikishore Tadigotla <harikishore.t@gmail.com>
2014-11-29 07:01:13 +0530

    minor

--- src/celldesigner/SamplePlugin.java
+++ src/celldesigner/SamplePlugin.java
@@ -21,6 +21,8 @@ import jp.sbi.celldesigner.plugin.PluginReaction;
 import jp.sbi.celldesigner.plugin.PluginSBase;
 import jp.sbi.celldesigner.plugin.PluginSpeciesAlias;
 
+import com.al.ReactantsFrame;
+
 public class SamplePlugin extends CellDesignerPlugin {
 	/**
 	 * Constructor
@@ -77,9 +79,44 @@ class SampleAction extends PluginAction {
 		pluginframe();
 	}
 
-	public void pluginframe() {
-		JFrame fr = new JFrame("Flux Based Scoring System");
-		fr.setSize(250, 250);
+	private void pluginframe() {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ReactantsFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ReactantsFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ReactantsFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ReactantsFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ReactantsFrame().setVisible(true);
+            }
+        });
+		
+	}
+
+	public void pluginframe1() {
+		JFrame fr = new ReactantsFrame();
+		
+		//JFrame fr = new JFrame("Flux Based Scoring System");
+		//fr.setSize(250, 250);
 		fr.addWindowListener(new BasicWindowMonitor());
 		Panel pnl = new Panel();
 		fr.add(pnl);
