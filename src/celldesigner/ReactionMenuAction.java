4cf1d63d0db781c1e6329fcab4835f2a7f3165fd
Harikishore Tadigotla <harikishore.t@gmail.com>
2014-12-05 21:59:30 +0530

    Incremental

--- src/celldesigner/ReactionMenuAction.java
+++ src/celldesigner/ReactionMenuAction.java
@@ -1,6 +1,12 @@
 package celldesigner;
 
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import jp.sbi.celldesigner.plugin.PluginAction;
 import jp.sbi.celldesigner.plugin.PluginListOf;
@@ -16,6 +22,30 @@ class ReactionMenuAction extends PluginAction {
 
 	public ReactionMenuAction(CustomPlugin _plugin) {
 		this.plugin = _plugin;
+		calculateScores();
+	}
+
+	private void calculateScores() {
+		PluginModel selectedModel = this.plugin.getSelectedModel();
+		Map<String, Integer> scoreMap = new HashMap();
+		
+		for (int i = 0; i < selectedModel.getNumReactions(); i++) {
+			PluginReaction reaction = selectedModel.getReaction(i);
+			int reactants = reaction.getNumReactants();
+			int modifiers = reaction.getNumModifiers();
+			List productList = getAllProducts(reaction);
+			
+			for (Iterator iterator = productList.iterator(); iterator.hasNext();) {
+				String product = (String) iterator.next();
+				
+				if(scoreMap.containsKey(product)){
+					scoreMap.put(product, scoreMap.get(product) - 1);
+				}else{
+					scoreMap.put(product, reactants + modifiers);
+				}
+			}
+		}
+
 	}
 
 	public void myActionPerformed(ActionEvent e) {
@@ -24,40 +54,51 @@ class ReactionMenuAction extends PluginAction {
 
 	public void showPluginframe() {
 
-            PluginModel selectedModel = this.plugin.getSelectedModel();
-            int reactionCount = selectedModel.getNumReactions();
-            
-            Object[][] modelData = new Object[reactionCount][8];
+		PluginModel selectedModel = this.plugin.getSelectedModel();
+		int reactionCount = selectedModel.getNumReactions();
 
-            for (int i = 0; i < reactionCount; i++) {
-                PluginReaction reaction = selectedModel.getReaction(i);
-				String reactionType = reaction.getReactionType();
-                String products = getAllProducts(reaction);
-                String reactants = GetAllReactants(reaction);
-                String modifiers = GetAllModifiers(reaction);
-                
-                modelData[i] = new String[]{reaction.getId(), reactionType, products, Integer.toString(reaction.getNumProducts()), 
-                		reactants, Integer.toString(reaction.getNumReactants()), modifiers, Integer.toString(reaction.getNumModifiers())};
-            }
-            String[] columnNames = new String[]{"Reaction Name","Reaction Types","Products", "No. Of Products", 
-            		"Reactants", "No. Of Reactants","Modifiers", "No.Of Modifiers"};
+		Object[][] modelData = new Object[reactionCount][8];
 
-			CellDesignerPluginFrame fr = new CellDesignerPluginFrame(modelData, columnNames);
+		for (int i = 0; i < reactionCount; i++) {
+			PluginReaction reaction = selectedModel.getReaction(i);
+			String reactionType = reaction.getReactionType();
+			List products = getAllProducts(reaction);
+			String reactants = GetAllReactants(reaction);
+			String modifiers = GetAllModifiers(reaction);
 
-            fr.addWindowListener(new BasicWindowMonitor());
+			modelData[i] = new String[] { reaction.getId(), reactionType,
+					Arrays.asList(products).toString(),
+					Integer.toString(reaction.getNumProducts()), reactants,
+					Integer.toString(reaction.getNumReactants()), modifiers,
+					Integer.toString(reaction.getNumModifiers()) };
 
-            fr.setVisible(true);
+			// fOR EACH PRODUCT IN A REACTION SCORE OF PRODUCT = NO. OF
+			// REACTANTS + NO. OF PRODUCTS
+			// IF A PRODUCT IS OCCURING IN ANOTHER REACTION, REDUCE THE SCORE BY
+			// 1
+		}
+		String[] columnNames = new String[] { "Reaction Name",
+				"Reaction Types", "Products", "No. Of Products", "Reactants",
+				"No. Of Reactants", "Modifiers", "No.Of Modifiers" };
+
+		CellDesignerPluginFrame fr = new CellDesignerPluginFrame(modelData,
+				columnNames);
+
+		fr.addWindowListener(new BasicWindowMonitor());
+
+		fr.setVisible(true);
 	}
 
-	public String getAllProducts(PluginReaction reaction) {
+	public List getAllProducts(PluginReaction reaction) {
+		List productList = new ArrayList();
 		int num = reaction.getNumProducts();
 		String products = new String();
 		for (int i = 0; i < num; i++) {
-			products = (String) products + ":"
-					+ reaction.getProduct(i).getSpeciesInstance().getName();
+			productList.add(reaction.getProduct(i).getSpeciesInstance()
+					.getName());
 		}
 
-		return products;
+		return productList;
 	}
 
 	public String GetAllReactants(PluginReaction reaction) {
